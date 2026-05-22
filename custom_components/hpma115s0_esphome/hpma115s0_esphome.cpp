21d77e4f4484a7b80a1ccf5e8810b22f46d33e6a
Mamath <15641879+Mamath2000@users.noreply.github.com>
2026-05-22 19:31:24 +0200

    Handle manual HPMA initialization at boot

--- custom_components/hpma115s0_esphome/hpma115s0_esphome.cpp
+++ custom_components/hpma115s0_esphome/hpma115s0_esphome.cpp
@@ -38,6 +38,9 @@ void HPMA115S0Component::dump_config() {
     } else {
       ESP_LOGCONFIG(TAG, "Sensor found after %i tries. Will read and log values.", setupTries);
     }
+  } else if (this->get_update_interval() == SCHEDULER_DONT_RUN) {
+    ESP_LOGW(TAG,
+             "Initial HPMA probe did not answer during setup. Waiting for the manual measurement cycle to initialize the sensor.");
   } else {
     ESP_LOGE(TAG, "Error! HPMA115S0 Particle Sensor not found! Failed with: %i AND %i.", setup_SAS, setup_SM);
   }
